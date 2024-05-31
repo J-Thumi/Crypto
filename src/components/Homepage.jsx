277bc1870040fc89509919453e987519d7191a08
Josphat Waweru Thumi <thumijosphat47@gmail.com>
2024-05-31 14:00:20 +0300

    error solved

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -3,8 +3,17 @@ import millify from 'millify'
 import {Typography,Row,Col,Statistic} from 'antd'
 import {Link} from 'react-router-dom'
 
+import { useGetCryptosQuery } from '../services/CryptoApi'
+
+
 const {Title}=Typography
 const Homepage = () => {
+
+    const {data,isFetching} = useGetCryptosQuery()
+   const globalStats=data?.data?.stats
+
+
+    if (isFetching) return 'Loading...'
   return (
     <div>
         
@@ -13,7 +22,7 @@ const Homepage = () => {
      
      <Row>
 
-        <Col span={12} ><Statistic title="Total Cryptocurrencies" value="5" /></Col>
+        <Col span={12} ><Statistic title="Total Cryptocurrencies" value={globalStats.total}/></Col>
         <Col span={12} ><Statistic title="Total Exchanges" value="5" /></Col>
         <Col span={12} ><Statistic title="Total Market Cap" value="5" /></Col>
         <Col span={12} ><Statistic title="Total 24h Volume" value="5" /></Col>
---
+++ src/services/CryptoApi.js
@@ -0,0 +1,30 @@
+import {createApi,fetchBaseQuery} from '@reduxjs/toolkit/query/react'
+
+
+
+const cryptoApiHeaders={
+    'X-RapidAPI-Key': '8886828998msh5349933319e72c1p1a95b6jsn7dd4337d7a2a',
+    'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+    
+}
+
+
+const baseUrl='https://coinranking1.p.rapidapi.com'
+
+
+const createRequest=(url)=>({url, headers: cryptoApiHeaders})
+
+export const cryptoApi=createApi({
+    reducerPath:'cryptoApi',
+    baseQuery: fetchBaseQuery({baseUrl}),
+    endpoints: (builder)=>({
+        getCryptos: builder.query({
+            query:()=>createRequest('/coins')
+        })
+    })
+})
+
+
+export const {
+    useGetCryptosQuery,
+} = cryptoApi
